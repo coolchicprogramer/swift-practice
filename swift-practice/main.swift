83795cce8b9b0d6a5a467c32db4d055eebc6d8b1
coolchicprogramer <coolchicprogramer@gmail.com>
2017-11-26 21:59:54 +0900

    add skeleton project

---
+++ swift-practice/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  swift-practice
+//
+//  Created by user on 2017/11/26.
+//  Copyright © 2017年 coolchic. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
